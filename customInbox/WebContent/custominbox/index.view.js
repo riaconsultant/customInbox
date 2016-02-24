1e5be80e92ad1c06db2b5c6011672ad1f98cb80a
riaconsultant <manoj.chaurasiya@gmail.com>
2016-02-24 14:49:07 +0530

    first Commit

---
+++ customInbox/WebContent/custominbox/index.view.js
@@ -0,0 +1,19 @@
+sap.ui.jsview("custominbox.index", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf custominbox.index
+	*/ 
+	getControllerName : function() {
+		return "custominbox.index";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf custominbox.index
+	*/ 
+	createContent : function(oController) {
+
+	}
+
+});
