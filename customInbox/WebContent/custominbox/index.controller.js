238d29bc4a84edea355b9ddd92b54282e4cc00c6
riaconsultant <manoj.chaurasiya@gmail.com>
2016-02-25 14:41:19 +0530

    add config file

--- customInbox/WebContent/custominbox/index.controller.js
+++ customInbox/WebContent/custominbox/index.controller.js
@@ -5,14 +5,135 @@ sap.ui.controller("custominbox.index", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf custominbox.index
 */
-//	onInit: function() {
-//
-//	},
-
+	oModel:new sap.ui.model.json.JSONModel(),
+	oData:"",
+	onInit: function() {
+		othis=this;
+		selectedKey="ne";
+		oColumn="";
+		oData=getAllBPMTasks(selectedKey);
+		var view=this.getView();
+		oTable=this.getView().getContent()[0].getContent()[0].getContent()[0];
+		oDropDown=oTable.getExtension()[0].getAggregation("rows")[0].getCells()[0].getContent()[0];
+		othis.oModel.setData(oData);
+		sap.ui.getCore().setModel(othis.oModel);
+		var ua = navigator.userAgent;
+	     browerEx = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+	     browerEx = browerEx[2] ? [browerEx[1], browerEx[2]] : [navigator.appName, navigator.appVersion, '-?'];
+	 if ((tem = ua.match(/version\/(\d+)/i)) !=null) {
+	 browerEx.splice(1,1,tem[1]);
+	 }
+	},
+	callBPMTasks:function(oEvent){
+		var linkTemplate=new sap.ui.commons.Link({textAlign :"Left",press:function(oEvent){
+				var oRow=oEvent.getSource();
+				callBPMDialog(parseInt(oRow.getBindingContext().sPath.split("/")[1]));
+			}}).bindProperty("text", "TaskTitle");
+		var textTemplate=new sap.ui.commons.TextView({textAlign :"Left"}).bindProperty("text","TaskTitle");
+		oColumn=oTable.getColumns()[0];
+		selectedKey=oEvent.getSource().getSelectedKey();
+		var oData=getAllBPMTasks(selectedKey);
+		var count=oData.length;
+		othis.oModel.setData(oData);
+		othis.oModel.refresh(true);
+		if(selectedKey=="ne"){
+			oColumn.setTemplate(linkTemplate);
+			this.setHeaderCount("Open Tasks ( "+count+" )");
+		}
+		else{
+			oColumn.setTemplate(textTemplate);
+			this.setHeaderCount("Completed Tasks ( "+count+" )");
+		}
+		oTable.rerender();
+	 	sap.ui.getCore().setModel(othis.oModel);
+	},
+	setHeaderCount:function(arg){
+		var pId=$(".commonPanel").attr('id');
+		$("#"+pId+"-title").text(arg);
+	},
+	changeDateFormat:function(date){
+		if(browerEx[0]==="Chrome"){
+		var time=date.toLocaleString().split(",")[1];
+		
+		var changedDate=othis.getCorrectFormat(date);
+  		 var formDate = changedDate+" "+time;
+	}
+		if(browerEx[0]==="Firefox"){
+			var formDate = date.toLocaleFormat();
+		}
+		if(browerEx[0]==="Trident"||browerEx[0]==="Safari"){
+			var time=date.toLocaleTimeString();
+			var changedDate=othis.getCorrectFormat(date);
+			var formDate = changedDate+" "+time;
+		}
+  		 return formDate;
+	},
+	refreshTableData:function(oEvent){
+		var oData="";
+		if(selectedKey=="ne"){
+			oData=getAllBPMTasks(selectedKey);
+			this.setHeaderCount("Open Tasks ( "+oData.length+" )");
+		}
+		else{
+			oData=getAllBPMTasks(selectedKey);
+			this.setHeaderCount("Completed Tasks ( "+oData.length+" )");
+		}
+		oTable.rerender();
+		othis.oModel.setData(oData);
+		othis.oModel.refresh(true);
+	 	sap.ui.getCore().setModel(othis.oModel);
+	},
+	getCorrectFormat:function(date){
+		var dd=date.getUTCDate();
+		var month = date.getUTCMonth()+1;
+  		 var mm ;
+  		 var yy = date.getUTCFullYear();
+		
+  		 switch(month){
+		
+		 case 1 : mm = "Jan";
+		 break;
+		
+		 case 2 : mm = "Feb";
+		 break;
+		
+		 case 3 : mm = "Mar";
+		 break;
+		
+		 case 4 : mm = "Apr";
+		 break;
+		
+		 case 5 : mm = "May";
+		 break;
+		
+		 case 6 : mm = "Jun";
+		 break;
+		
+		 case 7 : mm = "Jul";
+		 break;
+		
+		 case 8 : mm = "Aug";
+		 break;
+		
+		 case 9 : mm = "Sep";
+	 	 break;
+		
+		 case 10 : mm = "Oct";
+		 break;
+		
+		 case 11 : mm = "Nov";
+		 break;
+		
+		 case 12 : mm = "Dec";
+		 break;
+		
+		 }
+  		 return mm +" "+dd+", "+yy;
+	},
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 * (NOT before the first rendering! onInit() is used for that one!).
-* @memberOf custominbox.index
+* @memberOf bpminbox.index
 */
 //	onBeforeRendering: function() {
 //
@@ -21,18 +142,10 @@ sap.ui.controller("custominbox.index", {
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
-* @memberOf custominbox.index
+* @memberOf bpminbox.index
 */
-//	onAfterRendering: function() {
-//
-//	},
-
-/**
-* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-* @memberOf custominbox.index
-*/
-//	onExit: function() {
-//
-//	}
+	onAfterRendering: function() {
+		this.setHeaderCount("Open Tasks ( "+oData.length+" )");
+	}
 
 });
--- customInbox/WebContent/custominbox/index.view.js
+++ customInbox/WebContent/custominbox/index.view.js
@@ -13,7 +13,123 @@ sap.ui.jsview("custominbox.index", {
 	* @memberOf custominbox.index
 	*/ 
 	createContent : function(oController) {
-
+		var oBaseVerticalLayout = new sap.ui.commons.layout.VerticalLayout({width : "100%"});
+		oBaseVerticalLayout.addStyleClass("midDisplayClass");
+		
+		var oBasePanel = new sap.ui.commons.Panel({
+			text: "Open Task",
+			width : "100%",
+			showCollapseIcon : false, 
+			tooltip : ""
+		});
+		oBasePanel.addStyleClass("commonPanel").addStyleClass("panel-default");
+		
+		var oBrandDropdown = new sap.ui.commons.DropdownBox({
+			items : [new sap.ui.core.ListItem({text:"Open Task", key:"ne"}),
+			         new sap.ui.core.ListItem({text:"Completed Task", key:"eq"})],
+			change : function(event){oController.callBPMTasks(event); }});
+		var oTaskLabel=new sap.ui.commons.Label({text:"All Tasks: "});
+		var oRefreshButton=new sap.ui.commons.Button({tooltip:"Refresh",icon : "sap-icon://refresh",press:function(oEvent){
+			oController.refreshTableData(oEvent);
+		}});
+		var oTable = new sap.ui.table.Table({
+			visibleRowCount: 10,
+			selectionMode: sap.ui.table.SelectionMode.None,
+			selectionBehavior : sap.ui.table.SelectionBehavior.RowSelector,
+			extension: [
+			   new sap.ui.commons.layout.MatrixLayout({columns: 4, widths:["12%","15%","12%","61%"]}).createRow(oTaskLabel,oBrandDropdown,oRefreshButton,"")
+					]
+		});
+		
+		oTable.addColumn(new sap.ui.table.Column({
+			label: new sap.ui.commons.Label({text: "Task Title",textAlign :"Left"}),
+			template: new sap.ui.commons.Link({textAlign :"Left",press:function(oEvent){var oRow=oEvent.getSource();
+			callBPMDialog(parseInt(oRow.getBindingContext().sPath.split("/")[1]));}}).bindProperty("text", "TaskTitle"),
+			sortProperty: "TaskTitle",
+			filterProperty: "TaskTitle",
+			visible: true,
+			width:"30%"
+		}));
+		
+		oTable.addColumn(new sap.ui.table.Column({
+			label: new sap.ui.commons.Label({text: "Creation Date",textAlign :"Left"}),
+			template: new sap.ui.commons.TextView({textAlign :"Left"}).bindProperty("text", 
+					{
+				 parts: [
+				 {path: "CreatedOn", type: new
+				 sap.ui.model.type.String()}
+				 ],
+				 formatter: function(CreatedOn){
+				 var value="";
+				 if(CreatedOn)
+				 {
+				 var date = new Date(parseInt(CreatedOn.substring(6,19)));
+				 value=oController.changeDateFormat(date);
+				 }
+				 else{
+				 value="";
+				 }
+									      	     		 	            
+				 return value;
+				 }
+				 }		
+			),
+			resizable:false,
+			sortProperty: "CreatedOn",
+			filterProperty: "CreatedOn",
+			width:"20%"
+		}));
+		
+		oTable.addColumn(new sap.ui.table.Column({
+			label: new sap.ui.commons.Label({text: "Due Date",textAlign :"Left"}),
+			template: new sap.ui.commons.TextView({textAlign :"Left"}).bindProperty("text", {
+				 parts: [
+						 {path: "CompletionDeadLine", type: new
+						 sap.ui.model.type.String()}
+						 ],
+						 formatter: function(CompletionDeadLine){
+						 var value="";
+						 if(CompletionDeadLine)
+						 {
+						 var date = new Date(parseInt(CompletionDeadLine.substring(6,19)));
+						 value=oController.changeDateFormat(date);
+						 }
+						 else{
+						 value="";
+						 }
+											      	     		 	            
+						 return value;
+						 }
+						 }),
+			resizable:false,
+			width:"20%",
+			sortProperty: "CompletionDeadLine",
+			filterProperty: "CompletionDeadLine"
+			
+		}));
+		
+		oTable.addColumn(new sap.ui.table.Column({
+			label: new sap.ui.commons.Label({text: "Status",textAlign :"Left"}),
+			template: new sap.ui.commons.TextView({textAlign :"Left"}).bindProperty("text", "Status"),
+			resizable:false,
+			width:"15%",
+			sortProperty: "Status",
+			filterProperty: "Status"
+		}));
+		
+		oTable.addColumn(new sap.ui.table.Column({
+			label: new sap.ui.commons.Label({text: "Priority", textAlign :"Left"}),
+			template: new sap.ui.commons.TextView({textAlign :"Left"}).bindProperty("text", "Priority"),
+			resizable:false,
+			width:"15%",
+			sortProperty: "Priority",
+			filterProperty: "Priority"
+		}));
+		oTable.bindRows("/");
+		oBasePanel.addContent(oTable);		
+		oBaseVerticalLayout.addContent(oBasePanel);
+		
+		return oBaseVerticalLayout;
 	}
 
 });
